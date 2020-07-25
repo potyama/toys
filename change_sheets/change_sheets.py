cfe27040a70af3fdeebb0f6fe3a3db09e2681c3f
potyama <kamikaze117117@gmail.com>
2020-07-25 23:02:04 +0900

    [Add]席替え結果の表示にかかる時間をrandomに設定

--- change_sheets/change_sheets.py
+++ change_sheets/change_sheets.py
@@ -6,10 +6,11 @@ N = int(input('Enter number of students: '))
 students = list(range(1, N+1))
 
 random.shuffle(students)
-
 for i, student in enumerate(students):
-    if i % 7 == 0 and i != 0:
+    if i % 7 == 0:
         print()
-    print(" {} |".format(student), end='', flush = True)
-    time.sleep(0.1)
 
+    print("{0:3d}|".format(student), end='', flush = True)
+    time.sleep(random.random())
+
+print()
